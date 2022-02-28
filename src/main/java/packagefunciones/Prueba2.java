a108b1ab486813e221a532315f2b03ae7bcb1b67
Stephanny Diana Alejandra Guevara Bocanegra <sdguevara85@misena.edu.co>
2022-02-27 19:13:48 -0500

    Se añadio parte de prueba2 (falta importar ciertos archivos)

---
+++ src/main/java/packagefunciones/Prueba2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package packagefunciones;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DIANA GUEVARA
+ */
+import static packagefunciones.Clase1.Llenarmatriz;
+import static packagefunciones.Clase1.Matexamreprobados;
+import static packagefunciones.Clase1.Sacarpromedio;
+import static packagefunciones.Clase1.calmaypromycantexamregul;
+public class Prueba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float notas[][];
+        float promedio = 0;
+        notas=Llenarmatriz();
+        promedio=Sacarpromedio(notas);
+        
+        calmaypromycantexamregul(notas,promedio);
+        
+        Matexamreprobados(notas);
+        
+        Estmayordesquimica(notas);
+    }
+    
+}
